c745e32c66436c867721f9b7ac65acd750e419fb
marekkocemba <marekkocembapos@gmail.com>
2025-03-07 00:14:31 +0100

    Added Spring framework empty project.

---
+++ empik-backend/src/main/java/com/empik/empik_backend/EmpikBackendApplication.java
@@ -0,0 +1,13 @@
+package com.empik.empik_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpikBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpikBackendApplication.class, args);
+	}
+
+}
